bbf987d1cf211c8de466fbd06c0565fa8b58a552
L1 L31 <wellming.li@howzhi.com>
2019-03-28 00:53:35 +0800

    fix comment.

--- src/WorkerInterface.php
+++ src/WorkerInterface.php
@@ -13,11 +13,6 @@ interface WorkerInterface
 
     /**
      * Worker执行返回码：重试
-     *   返回可选参数：
-     *     * delay: 延迟{delay}秒执行
-     *     * pri: 任务优先级
-     *     * ttr: 任务执行的超时时间
-     *   如未指定可选参数，则沿用原有job的值
      */
     const RETRY = 'retry';
 
